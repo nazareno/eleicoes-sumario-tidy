639ebd051b2ad320c7e2c4324ce01d8c9c5f8e28
gileadekelvin <gileadekelvin@gmail.com>
2018-10-01 20:57:40 -0300

    Adiciona dados dos votos para presidente com informações tratadas de cargo e nome do candidato

---
+++ transform.R
@@ -0,0 +1,21 @@
+dados_presidente_partido <- function(data_path){
+    # data_path - caminho para os dados votos_tidy_long
+    library(tidyverse)
+    library(readr)
+    
+    votos <- read_csv(data_path)
+    
+    votos_presidente <- votos %>%
+        mutate(coligacao = sapply(str_extract_all(candidato, "\\b[A-Z]+\\b|PCdoB|PDCdoB|PTdoB"), paste, collapse= ' ')) %>% 
+        mutate(partido = gsub("([A-Za-z]+).*", "\\1", coligacao)) %>%
+        rowwise() %>% 
+        mutate(nome = gsub(paste0("\\", partido, ".*", "|\\("), "", candidato)) %>% 
+        na.omit() %>% 
+        select(estado, nome, partido, coligacao, votos, cargo, ano, turno)
+    
+    return(votos_presidente)
+}
+
+data_path <- here("data/votos_tidy_long.csv")
+votos <- dados_presidente_partido(data_path)
+write.csv(votos, here("data/votos_presidente_partido.csv"), row.names = FALSE)
