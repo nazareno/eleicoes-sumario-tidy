7f458e72c132356ecb108e417f52d027181bde04
gileadekelvin <gileadekelvin@gmail.com>
2018-10-02 22:03:30 -0300

    Adiciona dados da eleição de 2014 considerando os estados

--- code/transform.R
+++ code/transform.R
@@ -59,6 +59,33 @@ dados_presidente_disputa_turno <- function(data_path) {
     return(votos_turno)
 }
 
+dados_presidente_estado_historico <- function(data_path, ano_eleicao) {
+    
+    votos <- read_csv(data_path)
+    
+    votos_presidente <- votos %>%
+        mutate(coligacao = sapply(str_extract_all(candidato, "\\b[A-Z]+\\b|PCdoB|PDCdoB|PTdoB"), paste, collapse= ' ')) %>% 
+        mutate(partido = gsub("([A-Za-z]+).*", "\\1", coligacao)) %>%
+        rowwise() %>% 
+        mutate(nome = gsub(paste0("\\", partido, ".*", "|\\("), "", candidato)) %>% 
+        na.omit() %>% 
+        filter(estado != "TOTAL") %>%
+        
+        mutate(estado = ifelse(grepl("Federal", estado), "Distrito Federal", estado)) %>% 
+        select(estado, ano, turno, partido, votos)
+        
+    votos_estado_historico <- votos_presidente %>% 
+        filter(partido %in% c("PT", "PSDB")) %>% 
+        filter(ano == ano_eleicao, turno == "Turno 2") %>% 
+        
+        group_by(estado) %>% 
+        mutate(total = sum(votos)) %>% 
+        mutate(porcentagem = (votos / total) * 100) %>% 
+        ungroup()
+    
+    return(votos_estado_historico)
+}
+
 library(here)
 data_path <- here("data/votos_tidy_long.csv")
 votos <- dados_presidente_partido(data_path)
@@ -69,3 +96,6 @@ write.csv(votos_agrupado, here("data/votos_presidente_agrupado.csv"), row.names 
 
 votos_turno <- dados_presidente_disputa_turno(data_path)
 write.csv(votos_turno, here("data/votos_presidente_segundo_turno.csv"), row.names = FALSE)
+
+votos_estado_ano <- dados_presidente_estado_historico(data_path, 2014)
+write.csv(votos_estado_ano, here("data/votos_presidente_estado_ano.csv"), row.names = FALSE)
